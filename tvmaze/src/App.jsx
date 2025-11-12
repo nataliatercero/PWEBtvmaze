bbee3762a399fded10952100a64eeb1e253f563d
nataliatercero <nataliatercero2005@gmail.com>
2025-11-12 01:36:44 +0100

    implementar formulario y resultados simulados con estilos base
    Se implementa la búsqueda simulada mediante formulario controlado con useState.
    Se añaden estilos básicos para estructura y legibilidad.

--- tvmaze/src/App.jsx
+++ tvmaze/src/App.jsx
@@ -3,25 +3,55 @@ import "./App.css";
 
 export default function App() {
   const [busqueda, setBusqueda] = useState("");
+  const [resultados, setResultados] = useState([]); // esto será la lista de series
 
   const handleChange = (event) => {
-    console.log("Valor introducido:", event.target.value);
     setBusqueda(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // para que no recargue la página
+    console.log("Buscando serie:", busqueda);
+
+    if (busqueda.trim() === ""){
+      setResultados([]);
+      return
+    }
+
+    setResultados([
+      { id: 1, nombre: `Resultado de prueba para "${busqueda}"`}
+    ])
   }
 
   return (
     <div className = "app">
       <h1>Buscador de series</h1>
 
+      <form onSubmit = {handleSubmit}>
       <input
-        id = "searInput"
+        id = "searchInput"
         type = "text"
         placeholder = "Escribe una serie..."
         value = {busqueda}
-        onCahnge = {handleCahnge}
+        onChange = {handleChange}
       />
+      <button type = "submit">Buscar</button>
+      </form>
 
-      <p>Texto de búsqueda: {busqueda}</p>  
+      <p className = "search-text">Texto de búsqueda: {busqueda}</p>
+
+      <section className = "resultados">
+        <h2>Resultados</h2>
+        {resultados.length === 0 ? (
+          <p>No hay resultados</p>
+        ) : (
+          <ul>
+            {resultados.map((r) => (
+              <li key = {r.id}>{r.nombre}</li>
+            ))}
+          </ul>
+        )}
+      </section> 
     </div>
   )
 }
