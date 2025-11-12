0fa31fe24aff08768f6f1e006a1675a2d554667e
nataliatercero <nataliatercero2005@gmail.com>
2025-11-12 01:00:35 +0100

    Estructura inicial y componente App con input
    Se crean los componentes vacíos y se implementa un input con useSate y onChange en App.jsx

--- tvmaze/src/App.jsx
+++ tvmaze/src/App.jsx
@@ -1,15 +1,27 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [busqueda, setBusqueda] = useState("");
+
+  const handleChange = (event) => {
+    console.log("Valor introducido:", event.target.value);
+    setBusqueda(event.target.value);
+  }
 
   return (
-    <>
-    <div>
-      
+    <div className = "app">
+      <h1>Buscador de series</h1>
+
+      <input
+        id = "searInput"
+        type = "text"
+        placeholder = "Escribe una serie..."
+        value = {busqueda}
+        onCahnge = {handleCahnge}
+      />
+
+      <p>Texto de búsqueda: {busqueda}</p>  
     </div>
-    </>
   )
 }
-
-export default App
